5cfcab8ef6ad389a016bb3662e047e613d88199f
max <max@DESKTOP-FESOMTJ>
2025-03-17 16:02:25 +0200

    Homework.Java-1.3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args )
+    {
+        Persoana persoana = new Persoana("Ion",25,"Pitesti");
+        System.out.println(persoana.toString());
+    }
+}
